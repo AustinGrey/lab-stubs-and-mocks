d1dfd4e31963a2654a1a7d2fe8a700720fac1454
AustinGrey <corey.crapo@gmail.com>
2020-02-03 19:14:59 -0700

    GREEN: test is passing

--- src/main/java/util/StringProcessor.java
+++ src/main/java/util/StringProcessor.java
@@ -3,8 +3,16 @@ package util;
 public class StringProcessor {
 
     public String capitalizeName(String name){
-        
-        return "";
+        String[] parts = name.toLowerCase().split(" ");
+        String output = "";
+        // Uppercase first letter in each part
+        for(int i=0; i<parts.length; i++){
+            output += parts[i].substring(0, 1).toUpperCase() + parts[i].substring(1);
+            if (i != (parts.length - 1)){
+                output += " ";
+            }
+        }
+        return output.toString();
     }
 
 }
